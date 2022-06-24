180ce1ae9e15c969d11c9801567bc2cdd23e2fe5
William Bryce Angell <bryce.angell@resi.io>
2022-06-24 16:32:11 -0500

    add newGame functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import "./App.css";
+import cardSources, { CardSource } from "./constants/cardSources";
+
+type Card = CardSource & { id: string };
 
 function App() {
+  const [cards, setCards] = useState<Array<Card>>([]);
+  const [turns, setTurns] = useState(0);
+
+  const newGame = () => {
+    const shuffledCards: Array<Card> = [...cardSources, ...cardSources]
+      .sort(() => Math.random() - 0.5)
+      .map((cardSource) => ({ ...cardSource, id: crypto.randomUUID() }));
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={newGame}>New Game</button>
     </div>
   );
 }
---
+++ src/constants/cardSources.ts
@@ -0,0 +1,10 @@
+export type CardSource = { src: string };
+
+export default [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];

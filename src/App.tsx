86a337365c2bd485aa20b3775166f2f805d1bff0
William Bryce Angell <bryce.angell@resi.io>
2022-06-24 17:15:32 -0500

    add handler for user choices

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import cardSources, { CardSource } from "./constants/cardSources";
@@ -8,6 +8,8 @@ export type CardSourceWithId = CardSource & { id: string };
 function App() {
   const [cards, setCards] = useState<Array<CardSourceWithId>>([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState<string | null>(null);
+  const [choiceTwo, setChoiceTwo] = useState<string | null>(null);
 
   const newGame = () => {
     const shuffledCards: Array<CardSourceWithId> = [
@@ -20,13 +22,28 @@ function App() {
     setTurns(0);
   };
 
+  const handleClick = (src: string) =>
+    !choiceOne ? setChoiceOne(src) : setChoiceTwo(src);
+
+  useEffect(() => {
+    if (!choiceTwo) return;
+    console.log(choiceOne === choiceTwo);
+    nextTurn();
+  }, [choiceTwo]);
+
+  const nextTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((turns) => turns + 1);
+  };
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={newGame}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <Card key={card.id} src={card.src} />
+          <Card key={card.id} src={card.src} handleClick={handleClick} />
         ))}
       </div>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,14 +2,20 @@ import "./Card.css";
 
 interface Props {
   src: string;
+  handleClick: (src: string) => void;
 }
 
-const Card: React.FC<Props> = ({ src }) => {
+const Card: React.FC<Props> = ({ src, handleClick }) => {
   return (
     <div className="card">
       <div>
         <img src={src} className="front" alt="card front" />
-        <img src="/img/cover.png" className="back" alt="card back" />
+        <img
+          src="/img/cover.png"
+          className="back"
+          alt="card back"
+          onClick={() => handleClick(src)}
+        />
       </div>
     </div>
   );
